7f26904c94549331a0031517f885a01c8eb92174
Alex Garcia Amezcua <alex.garciaa@estudiants.urv.cat>
2024-06-15 14:01:11 +0200

    Centralized implementation all working.

--- centralized.py
+++ centralized.py
@@ -9,6 +9,10 @@ class StorageSystem(store_pb2_grpc.KeyValueStoreServicer):
     def __init__(self, master, addr=None):
         self.storage = {}
         self.delay = 0
+        self.slowed = False
+        self.consistency_file = f"centralized_recovery_file.txt"
+
+        self.recover_from_failure(self.consistency_file)
 
         if not addr: # master node
             self.address = master
@@ -28,54 +32,98 @@ class StorageSystem(store_pb2_grpc.KeyValueStoreServicer):
         self.slave_stubs.append(stub)
         return store_pb2.Empty()
 
-    def put(self, request, context):
-        available = True
-        for stub in self.slave_stubs:
-            vote_request = store_pb2.VoteRequest(key=request.key)
-            vote_response = stub.canCommit(vote_request)
-            available = available & vote_response.vote
-        
-        time.sleep(self.delay)
-        if available:
-            commit = True
-            for stub in self.slave_stubs:
-                commit_request = store_pb2.CommitRequest(key=request.key, value=request.value)
-                commit_response = stub.doCommit(commit_request)
-                commit = commit & commit_response.committed
+    def recover_from_failure(self, file):
+        try:
+            with open(file, "r") as fp:
+                for line in fp:
+                    key, value = line.strip().split(':')
+                    self.storage[key] = value
+        except FileNotFoundError:
+            pass
 
-            if commit:
-                self.storage[request.key] = request.value
-                return store_pb2.PutResponse(success=True)
-            else:
-                for stub in self.slave_stubs:
-                    req = store_pb2.AbortRequest(key=request.key)
-                    stub.doAbort(req)
-                return store_pb2.PutResponse(success=False)
+    def write_to_consistency_file(self, file, key, value):
+        try:
+            with open(file, "r") as fp:
+                content = fp.readlines()
+        except FileNotFoundError:
+            with open(file, "w") as fp:
+                fp.write(f"{key}:{value}\n")
         else:
+            
+            key_exists = False
+            for i, line in enumerate(content):
+                if key in line:
+                    content[i] = f"{key}:{value}\n"
+                    key_exists = True
+                    break
+            
+            if not key_exists:
+                content.append(f"{key}:{value}\n")
+            
+            with open(file, "w") as fp:
+                fp.writelines(content)
+
+
+    def put(self, request, context):
+        if self.slowed:
             return store_pb2.PutResponse(success=False)
+        else:
+            available = True
+            for stub in self.slave_stubs:
+                vote_request = store_pb2.VoteRequest(key=request.key)
+                vote_response = stub.canCommit(vote_request)
+                available = available & vote_response.vote
+
+            if available:
+                commit = True
+                for stub in self.slave_stubs:
+                    commit_request = store_pb2.CommitRequest(key=request.key, value=request.value)
+                    commit_response = stub.doCommit(commit_request)
+                    commit = commit & commit_response.committed
+
+                if commit:
+                    self.storage[request.key] = request.value
+                    self.write_to_consistency_file(self.consistency_file, request.key, request.value)
+                    return store_pb2.PutResponse(success=True)
+                else:
+                    for stub in self.slave_stubs:
+                        req = store_pb2.AbortRequest(key=request.key)
+                        stub.doAbort(req)
+                    return store_pb2.PutResponse(success=False)
+            else:
+                return store_pb2.PutResponse(success=False)
 
     def get(self, request, context):
-        val = self.storage.get(request.key)
-        #time.sleep(self.delay)
-
-        if val:
-            return store_pb2.GetResponse(value=val, found=True)
-        else:
+        if self.slowed:
             return store_pb2.GetResponse(value=None, found=False)
+        else:
+            val = self.storage.get(request.key)
+            if val:
+                return store_pb2.GetResponse(value=val, found=True)
+            else:
+                return store_pb2.GetResponse(value=None, found=False)
         
     def slowDown(self, request, context):
-        self.delay += request.seconds
+        self.delay = request.seconds
+        self.slowed = True
+        while self.delay > 0:
+            time.sleep(1)
+            self.delay -= 1
         return store_pb2.SlowDownResponse(success=True)
 
     def restore(self, request, context):
         self.delay = 0
+        self.slowed = False
         return store_pb2.RestoreResponse(success=True)
 
     def canCommit(self, request, context):
+        if self.slowed:
+            return store_pb2.VoteResponse(vote=False)
         return store_pb2.VoteResponse(vote=True)
     
     def doCommit(self, request, context):
         self.storage[request.key] = request.value
+        #self.write_to_consistency_file(self.consistency_file, request.key, request.value)
         return store_pb2.CommitResponse(committed=True)
     
     def doAbort(self, request, context):
